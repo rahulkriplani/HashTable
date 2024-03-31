0659788ad19652916db671cc15f8cf47e64c2c65
rahulkriplani <54098163+rahulkriplani@users.noreply.github.com>
2024-03-31 16:37:45 +0530

    Create 4Sum_II.java

---
+++ Conclusion/4Sum_II.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i: nums1)
+            for(int j: nums2)
+                map.put((i+j),map.getOrDefault((i+j), 0)+1);
+        
+        int ans = 0;
+        for(int i: nums3)
+            for(int j: nums4)
+                ans += map.getOrDefault((-1*(i+j)), 0);
+        
+        return ans;
+                
+        
+    }
+}
